06f8295eb4d7716d035536999717311971cc1615
ScottSauers <123714540+ScottSauers@users.noreply.github.com>
2023-08-31 19:54:07 -0500

    Update feature engineering kaggle.py

--- feature engineering kaggle.py
+++ feature engineering kaggle.py
@@ -87,3 +87,64 @@ test_df = temp_df_imputed.loc[temp_df_imputed['Survived'].isna()]
 # Save to CSV
 train_df.to_csv('/Users/scott/Downloads/titanic/train_engineered.csv', index=False)
 test_df.to_csv('/Users/scott/Downloads/titanic/test_engineered.csv', index=False)
+
+
+### Below is the same thing but with no interaction / non-linear terms (good for tree models)
+
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+
+train_df = pd.read_csv('/Users/scott/Downloads/titanic/train.csv')
+test_df = pd.read_csv('/Users/scott/Downloads/titanic/test.csv')
+
+combined_df = pd.concat([train_df, test_df], sort=False, ignore_index=True)
+combined_df['Title'] = combined_df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
+
+title_mapping = {
+    "Mr": "Mr",
+    "Miss": "Miss",
+    "Mrs": "Mrs",
+    "Master": "Master",
+    "Dr": "Officer",
+    "Rev": "Rev",
+    "Col": "Officer",
+    "Major": "Officer",
+    "Mlle": "Miss",
+    "Mme": "Mrs",
+    "Don": "Royalty",
+    "Dona": "Royalty",
+    "Lady": "Royalty",
+    "Countess": "Royalty",
+    "Jonkheer": "Royalty",
+    "Sir": "Royalty",
+    "Capt": "Officer",
+    "Ms": "Miss"
+}
+combined_df['Title'] = combined_df['Title'].map(title_mapping)
+combined_df['FamilySize'] = combined_df['SibSp'] + combined_df['Parch'] + 1
+combined_df['IsAlone'] = 0
+combined_df.loc[combined_df['FamilySize'] == 1, 'IsAlone'] = 1
+combined_df['FamilyID'] = combined_df['Name'].apply(lambda x: x.split(',')[0]) + "_" + combined_df['FamilySize'].astype(str)
+family_counts = combined_df['FamilyID'].value_counts()
+combined_df['FamilyID'] = combined_df.apply(lambda row: 'Single' if family_counts[row['FamilyID']] == 1 or row['FamilySize'] == 1 else row['FamilyID'], axis=1)
+
+categorical_cols = ['Sex', 'Embarked', 'Title', 'FamilyID']
+one_hot_df = pd.get_dummies(combined_df[categorical_cols], columns=categorical_cols, drop_first=True)
+combined_df = pd.concat([combined_df, one_hot_df], axis=1)
+
+drop_columns = ['Name', 'Ticket', 'Cabin', 'Sex', 'Embarked', 'Title', 'FamilyID']
+combined_df.drop(drop_columns, axis=1, inplace=True)
+
+temp_df = combined_df.drop('Survived', axis=1)
+imputer = KNNImputer(n_neighbors=5)
+temp_df_imputed = imputer.fit_transform(temp_df)
+temp_df_imputed = pd.DataFrame(temp_df_imputed, columns=temp_df.columns)
+temp_df_imputed['Survived'] = combined_df['Survived']
+temp_df_imputed['AgeBand'] = pd.cut(temp_df_imputed['Age'], 5, labels=False)
+temp_df_imputed['FareBand'] = pd.qcut(temp_df_imputed['Fare'], 5, labels=False)
+train_df = temp_df_imputed.loc[temp_df_imputed['Survived'].notna()]
+test_df = temp_df_imputed.loc[temp_df_imputed['Survived'].isna()]
+
+train_df.to_csv('/Users/scott/Downloads/titanic/train_engineered_no_interaction.csv', index=False)
+test_df.to_csv('/Users/scott/Downloads/titanic/test_engineered_no_interaction.csv', index=False)
