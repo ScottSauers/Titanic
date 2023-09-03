2d0538a4d9e8a808e28f6035a48648711e8e7ab4
ScottSauers <123714540+ScottSauers@users.noreply.github.com>
2023-09-02 21:16:13 -0500

    Update feature engineering kaggle.py

--- feature engineering kaggle.py
+++ feature engineering kaggle.py
@@ -1,150 +1,138 @@
 import pandas as pd
-import numpy as np
-from sklearn.impute import KNNImputer
+from tpot import TPOTClassifier
+from sklearn.model_selection import train_test_split
+import json
+import pickle
 
-# Load datasets
-train_df = pd.read_csv('/Users/scott/Downloads/titanic/train.csv')
-test_df = pd.read_csv('/Users/scott/Downloads/titanic/test.csv')
+# Try to load feature-engineered training datasets
+try:
+    train_df = pd.read_csv('/Users/scott/Downloads/titanic/train_engineered.csv')
+    train_no_int_df = pd.read_csv('/Users/scott/Downloads/titanic/train_engineered_no_interaction.csv')
+except FileNotFoundError:
+    print("CSV file not found. Please check the file path.")
 
-# Combine DataFrames
-combined_df = pd.concat([train_df, test_df], sort=False, ignore_index=True)
-
-# Extract 'Title'
-combined_df['Title'] = combined_df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
-
-# Group titles
-title_mapping = {
-    "Mr": "Mr",
-    "Miss": "Miss",
-    "Mrs": "Mrs",
-    "Master": "Master",
-    "Dr": "Officer",
-    "Rev": "Rev",
-    "Col": "Officer",
-    "Major": "Officer",
-    "Mlle": "Miss",
-    "Mme": "Mrs",
-    "Don": "Royalty",
-    "Dona": "Royalty",
-    "Lady": "Royalty",
-    "Countess": "Royalty",
-    "Jonkheer": "Royalty",
-    "Sir": "Royalty",
-    "Capt": "Officer",
-    "Ms": "Miss"
+# Prepare datasets
+datasets = {
+    'xgb': train_df,
+    'rf': train_df,
+    'svc': train_df,
+    'knn': train_df,
+    'logreg': train_no_int_df
 }
-combined_df['Title'] = combined_df['Title'].map(title_mapping)
 
-# Create 'FamilySize'
-combined_df['FamilySize'] = combined_df['SibSp'] + combined_df['Parch'] + 1
-
-# Create 'IsAlone'
-combined_df['IsAlone'] = 0
-combined_df.loc[combined_df['FamilySize'] == 1, 'IsAlone'] = 1
-
-# Create 'FamilyID'
-combined_df['FamilyID'] = combined_df['Name'].apply(lambda x: x.split(',')[0]) + "_" + combined_df['FamilySize'].astype(str)
-
-# Count the occurrences of each FamilyID
-family_counts = combined_df['FamilyID'].value_counts()
-
-# Set FamilyID to 'Single' for those with a unique FamilyID or FamilySize of 1
-combined_df['FamilyID'] = combined_df.apply(lambda row: 'Single' if family_counts[row['FamilyID']] == 1 or row['FamilySize'] == 1 else row['FamilyID'], axis=1)
-
-# One-hot encode specific categorical columns, avoiding the dummy variable trap by using drop_first=True
-categorical_cols = ['Sex', 'Embarked', 'Title', 'FamilyID']
-one_hot_df = pd.get_dummies(combined_df[categorical_cols], columns=categorical_cols, drop_first=True)
-combined_df = pd.concat([combined_df, one_hot_df], axis=1)
-
-# Create interaction term between 'Sex' and 'Pclass'
-combined_df['Sex_Pclass'] = combined_df['Sex'] + "_" + combined_df['Pclass'].astype(str)
-
-# One-hot encode the interaction term, avoiding the dummy variable trap
-one_hot_interaction = pd.get_dummies(combined_df['Sex_Pclass'], drop_first=True)
-combined_df = pd.concat([combined_df, one_hot_interaction], axis=1)
-
-# Drop redundant or unnecessary columns
-drop_columns = ['Name', 'Ticket', 'Cabin', 'Sex', 'Embarked', 'Title', 'FamilyID', 'Sex_Pclass']
-combined_df.drop(drop_columns, axis=1, inplace=True)
-
-# Impute missing values with KNN
-temp_df = combined_df.drop('Survived', axis=1)
-imputer = KNNImputer(n_neighbors=5)
-temp_df_imputed = imputer.fit_transform(temp_df)
-temp_df_imputed = pd.DataFrame(temp_df_imputed, columns=temp_df.columns)
-
-# Add 'Survived' back
-temp_df_imputed['Survived'] = combined_df['Survived']
-
-# Create 'AgeBand' and 'FareBand'
-temp_df_imputed['AgeBand'] = pd.cut(temp_df_imputed['Age'], 5, labels=False)
-temp_df_imputed['FareBand'] = pd.qcut(temp_df_imputed['Fare'], 5, labels=False)
-
-# Split back into train and test
-train_df = temp_df_imputed.loc[temp_df_imputed['Survived'].notna()]
-test_df = temp_df_imputed.loc[temp_df_imputed['Survived'].isna()]
-
-# Save to CSV
-train_df.to_csv('/Users/scott/Downloads/titanic/train_engineered.csv', index=False)
-test_df.to_csv('/Users/scott/Downloads/titanic/test_engineered.csv', index=False)
-
-
-### Below is the same thing but with no interaction / non-linear terms (good for tree models)
-
-import pandas as pd
-import numpy as np
-from sklearn.impute import KNNImputer
-
-train_df = pd.read_csv('/Users/scott/Downloads/titanic/train.csv')
-test_df = pd.read_csv('/Users/scott/Downloads/titanic/test.csv')
-
-combined_df = pd.concat([train_df, test_df], sort=False, ignore_index=True)
-combined_df['Title'] = combined_df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
-
-title_mapping = {
-    "Mr": "Mr",
-    "Miss": "Miss",
-    "Mrs": "Mrs",
-    "Master": "Master",
-    "Dr": "Officer",
-    "Rev": "Rev",
-    "Col": "Officer",
-    "Major": "Officer",
-    "Mlle": "Miss",
-    "Mme": "Mrs",
-    "Don": "Royalty",
-    "Dona": "Royalty",
-    "Lady": "Royalty",
-    "Countess": "Royalty",
-    "Jonkheer": "Royalty",
-    "Sir": "Royalty",
-    "Capt": "Officer",
-    "Ms": "Miss"
+# Define TPOTClassifier parameters for each algorithm with early_stop
+tpot_params = {
+    'xgb': {
+        'generations': 50,
+        'population_size': 150,
+        'scoring': 'accuracy',
+        'verbosity': 2,
+        'early_stop': 4,
+        'cv': 10,
+        'mutation_rate': 0.7,
+        'crossover_rate': 0.2,
+        'warm_start': False,
+        'memory': 'auto',
+        'n_jobs': -1,
+        'max_eval_time_mins': 10,
+        'max_time_mins': 20,
+        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
+    },
+    'rf': {
+        'generations': 50,
+        'population_size': 150,
+        'scoring': 'accuracy',
+        'verbosity': 2,
+        'early_stop': 4,
+        'cv': 10,
+        'mutation_rate': 0.7,
+        'crossover_rate': 0.2,
+        'warm_start': False,
+        'memory': 'auto',
+        'n_jobs': -1,
+        'max_eval_time_mins': 10,
+        'max_time_mins': 20,
+        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
+    },
+    'svc': {
+        'generations': 50,
+        'population_size': 150,
+        'scoring': 'accuracy',
+        'verbosity': 2,
+        'early_stop': 4,
+        'cv': 10,
+        'mutation_rate': 0.7,
+        'crossover_rate': 0.2,
+        'warm_start': False,
+        'memory': 'auto',
+        'n_jobs': -1,
+        'max_eval_time_mins': 10,
+        'max_time_mins': 20,
+        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
+    },
+    'knn': {
+        'generations': 50,
+        'population_size': 150,
+        'scoring': 'accuracy',
+        'verbosity': 2,
+        'early_stop': 4,
+        'cv': 10,
+        'mutation_rate': 0.7,
+        'crossover_rate': 0.2,
+        'warm_start': False,
+        'memory': 'auto',
+        'n_jobs': -1,
+        'max_eval_time_mins': 10,
+        'max_time_mins': 20,
+        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
+    },
+    'logreg': {
+        'generations': 50,
+        'population_size': 150,
+        'scoring': 'accuracy',
+        'verbosity': 2,
+        'early_stop': 4,
+        'cv': 10,
+        'mutation_rate': 0.7,
+        'crossover_rate': 0.2,
+        'warm_start': False,
+        'memory': 'auto',
+        'n_jobs': -1,
+        'max_eval_time_mins': 10,
+        'max_time_mins': 20,
+        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
+    }
 }
-combined_df['Title'] = combined_df['Title'].map(title_mapping)
-combined_df['FamilySize'] = combined_df['SibSp'] + combined_df['Parch'] + 1
-combined_df['IsAlone'] = 0
-combined_df.loc[combined_df['FamilySize'] == 1, 'IsAlone'] = 1
-combined_df['FamilyID'] = combined_df['Name'].apply(lambda x: x.split(',')[0]) + "_" + combined_df['FamilySize'].astype(str)
-family_counts = combined_df['FamilyID'].value_counts()
-combined_df['FamilyID'] = combined_df.apply(lambda row: 'Single' if family_counts[row['FamilyID']] == 1 or row['FamilySize'] == 1 else row['FamilyID'], axis=1)
 
-categorical_cols = ['Sex', 'Embarked', 'Title', 'FamilyID']
-one_hot_df = pd.get_dummies(combined_df[categorical_cols], columns=categorical_cols, drop_first=True)
-combined_df = pd.concat([combined_df, one_hot_df], axis=1)
+# Dictionary to store the best models for each algorithm
+best_models = {}
 
-drop_columns = ['Name', 'Ticket', 'Cabin', 'Sex', 'Embarked', 'Title', 'FamilyID']
-combined_df.drop(drop_columns, axis=1, inplace=True)
+# Iterate over algorithms and perform automatic hyperparameter tuning using TPOT
+for algo, data in datasets.items():
+    model = TPOTClassifier(**tpot_params[algo])
+    
+    X = data.drop('Survived', axis=1)
+    y = data['Survived']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    
+    model.fit(X_train, y_train)
 
-temp_df = combined_df.drop('Survived', axis=1)
-imputer = KNNImputer(n_neighbors=5)
-temp_df_imputed = imputer.fit_transform(temp_df)
-temp_df_imputed = pd.DataFrame(temp_df_imputed, columns=temp_df.columns)
-temp_df_imputed['Survived'] = combined_df['Survived']
-temp_df_imputed['AgeBand'] = pd.cut(temp_df_imputed['Age'], 5, labels=False)
-temp_df_imputed['FareBand'] = pd.qcut(temp_df_imputed['Fare'], 5, labels=False)
-train_df = temp_df_imputed.loc[temp_df_imputed['Survived'].notna()]
-test_df = temp_df_imputed.loc[temp_df_imputed['Survived'].isna()]
+    best_models[algo] = model
 
-train_df.to_csv('/Users/scott/Downloads/titanic/train_engineered_no_interaction.csv', index=False)
-test_df.to_csv('/Users/scott/Downloads/titanic/test_engineered_no_interaction.csv', index=False)
+
+# Save full pipeline and hyperparameters to files
+for algo, model in best_models.items():
+    hyperparams = model.fitted_pipeline_[-1].get_params()  
+    hyperparams = {key.replace('classifier__', ''): value for key, value in hyperparams.items()}
+    json_filename = f'best_{algo}_params.json'
+    with open(json_filename, 'w') as f:
+        json.dump(hyperparams, f, indent=4)
+    
+    # Save the entire pipeline as well
+    pickle_filename = f'best_{algo}_pipeline.pkl'
+    with open(pickle_filename, 'wb') as f:
+        pickle.dump(model.fitted_pipeline_, f)
+
+    print(f"Best {algo} Hyperparameters saved to {json_filename}.")
+    print(f"Best {algo} Pipeline saved to {pickle_filename}.")
