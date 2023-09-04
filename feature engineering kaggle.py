3e0b3efe993641fa8c99278e5ec2af4ee3dcdbbc
ScottSauers <123714540+ScottSauers@users.noreply.github.com>
2023-09-03 23:26:24 -0500

    Update feature engineering kaggle.py

--- feature engineering kaggle.py
+++ feature engineering kaggle.py
@@ -1,138 +1,193 @@
 import pandas as pd
-from tpot import TPOTClassifier
+import numpy as np
+from sklearn.linear_model import BayesianRidge, LogisticRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import json
-import pickle
+import random
+from xgboost import XGBRegressor
+import xgboost as xgb
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.impute import KNNImputer
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
 
-# Try to load feature-engineered training datasets
-try:
-    train_df = pd.read_csv('/Users/scott/Downloads/titanic/train_engineered.csv')
-    train_no_int_df = pd.read_csv('/Users/scott/Downloads/titanic/train_engineered_no_interaction.csv')
-except FileNotFoundError:
-    print("CSV file not found. Please check the file path.")
 
-# Prepare datasets
-datasets = {
-    'xgb': train_df,
-    'rf': train_df,
-    'svc': train_df,
-    'knn': train_df,
-    'logreg': train_no_int_df
-}
+def load_and_combine_data(train_path, test_path):
+    train_df = pd.read_csv(train_path)
+    test_df = pd.read_csv(test_path)
+    combined_df = pd.concat([train_df, test_df], sort=False, ignore_index=True)
+    return combined_df
 
-# Define TPOTClassifier parameters for each algorithm with early_stop
-tpot_params = {
-    'xgb': {
-        'generations': 50,
-        'population_size': 150,
-        'scoring': 'accuracy',
-        'verbosity': 2,
-        'early_stop': 4,
-        'cv': 10,
-        'mutation_rate': 0.7,
-        'crossover_rate': 0.2,
-        'warm_start': False,
-        'memory': 'auto',
-        'n_jobs': -1,
-        'max_eval_time_mins': 10,
-        'max_time_mins': 20,
-        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
-    },
-    'rf': {
-        'generations': 50,
-        'population_size': 150,
-        'scoring': 'accuracy',
-        'verbosity': 2,
-        'early_stop': 4,
-        'cv': 10,
-        'mutation_rate': 0.7,
-        'crossover_rate': 0.2,
-        'warm_start': False,
-        'memory': 'auto',
-        'n_jobs': -1,
-        'max_eval_time_mins': 10,
-        'max_time_mins': 20,
-        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
-    },
-    'svc': {
-        'generations': 50,
-        'population_size': 150,
-        'scoring': 'accuracy',
-        'verbosity': 2,
-        'early_stop': 4,
-        'cv': 10,
-        'mutation_rate': 0.7,
-        'crossover_rate': 0.2,
-        'warm_start': False,
-        'memory': 'auto',
-        'n_jobs': -1,
-        'max_eval_time_mins': 10,
-        'max_time_mins': 20,
-        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
-    },
-    'knn': {
-        'generations': 50,
-        'population_size': 150,
-        'scoring': 'accuracy',
-        'verbosity': 2,
-        'early_stop': 4,
-        'cv': 10,
-        'mutation_rate': 0.7,
-        'crossover_rate': 0.2,
-        'warm_start': False,
-        'memory': 'auto',
-        'n_jobs': -1,
-        'max_eval_time_mins': 10,
-        'max_time_mins': 20,
-        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
-    },
-    'logreg': {
-        'generations': 50,
-        'population_size': 150,
-        'scoring': 'accuracy',
-        'verbosity': 2,
-        'early_stop': 4,
-        'cv': 10,
-        'mutation_rate': 0.7,
-        'crossover_rate': 0.2,
-        'warm_start': False,
-        'memory': 'auto',
-        'n_jobs': -1,
-        'max_eval_time_mins': 10,
-        'max_time_mins': 20,
-        'periodic_checkpoint_folder': '/Users/scott/Downloads/titanic',
+def add_title_feature(df):
+    df['Title'] = df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
+    title_mapping = {
+        "Mr": "Mr",
+        "Miss": "Miss",
+        "Mrs": "Mrs",
+        "Master": "Master",
+        "Dr": "Officer",
+        "Rev": "Rev",
+        "Col": "Officer",
+        "Major": "Officer",
+        "Mlle": "Miss",
+        "Mme": "Mrs",
+        "Don": "Royalty",
+        "Dona": "Royalty",
+        "Lady": "Royalty",
+        "Countess": "Royalty",
+        "Jonkheer": "Royalty",
+        "Sir": "Royalty",
+        "Capt": "Officer",
+        "Ms": "Miss"
     }
-}
+    df['Title'] = df['Title'].map(title_mapping)
+    return df
 
-# Dictionary to store the best models for each algorithm
-best_models = {}
+def add_family_features(df):
+    df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+    df['LastName'] = df['Name'].apply(lambda x: x.split(',')[0].strip())
+    df['FamilyID'] = df['LastName'] + "_" + df['FamilySize'].astype(str)
+    return df
 
-# Iterate over algorithms and perform automatic hyperparameter tuning using TPOT
-for algo, data in datasets.items():
-    model = TPOTClassifier(**tpot_params[algo])
+
+def add_new_features(df):
+    df['IsChild'] = df['Age'] < 18
+    df['IsElderly'] = df['Age'] > 60
+    df['IsAlone'] = df['FamilySize'] == 1
+    df['Deck'] = df['Cabin'].apply(lambda x: x[0] if pd.notna(x) else 'U')
+    df['TicketPrefix'] = df['Ticket'].apply(lambda x: x.split()[0] if len(x.split()) > 1 else 'None')
+    df['FarePerPerson'] = df['Fare'] / df['FamilySize']
+    df['NameLength'] = df['Name'].apply(len)
+    df['AgeGroup_Fare'] = df['Age'] * df['Fare']
+    df['Title_Age'] = df['Title'].astype(str) + "_" + df['Age'].astype(str)
+    df['Sex_Class'] = df['Sex'].astype(str) + "_" + df['Pclass'].astype(str)
     
-    X = data.drop('Survived', axis=1)
-    y = data['Survived']
+    # Adding interaction terms
+    df['Age_Class'] = df['Age'] * df['Pclass']
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # Create Age bins
+    df['Age_Bin'] = pd.cut(df['Age'], bins=[0, 18, 35, 50, 100], labels=['Child', 'YoungAdult', 'Adult', 'Elderly'])
     
-    model.fit(X_train, y_train)
-
-    best_models[algo] = model
-
-
-# Save full pipeline and hyperparameters to files
-for algo, model in best_models.items():
-    hyperparams = model.fitted_pipeline_[-1].get_params()  
-    hyperparams = {key.replace('classifier__', ''): value for key, value in hyperparams.items()}
-    json_filename = f'best_{algo}_params.json'
-    with open(json_filename, 'w') as f:
-        json.dump(hyperparams, f, indent=4)
+    # Create binned interaction terms
+    df['AgeBin_Class'] = df['Age_Bin'].astype(str) + "_" + df['Pclass'].astype(str)
+    df['AgeBin_Sex'] = df['Age_Bin'].astype(str) + "_" + df['Sex'].astype(str)
     
-    # Save the entire pipeline as well
-    pickle_filename = f'best_{algo}_pipeline.pkl'
-    with open(pickle_filename, 'wb') as f:
-        pickle.dump(model.fitted_pipeline_, f)
+    return df
 
-    print(f"Best {algo} Hyperparameters saved to {json_filename}.")
-    print(f"Best {algo} Pipeline saved to {pickle_filename}.")
+def one_hot_encoding(df, cols, drop_first=True):
+    one_hot_df = pd.get_dummies(df[cols], columns=cols, drop_first=drop_first)
+    df = pd.concat([df, one_hot_df], axis=1)
+    return df
+
+def remove_single_member_family_columns(df):
+    family_columns = [col for col in df.columns if 'FamilyID_' in col and col.endswith('_1')]
+    df.drop(family_columns, axis=1, inplace=True)
+    return df
+
+
+def feature_scaling(df, scaler=None):
+    # Store the "PassengerId" column temporarily if it exists in the DataFrame
+    passenger_id_column = None
+    if "PassengerId" in df.columns:
+        passenger_id_column = df["PassengerId"].copy()
+
+    # Drop the "PassengerId" column
+    if "PassengerId" in df.columns:
+        df.drop("PassengerId", axis=1, inplace=True)
+
+    # Apply scaling
+    if scaler is None:
+        scaler = StandardScaler()
+    numerical_cols = df.select_dtypes(include=[np.number]).columns.tolist()
+    numerical_cols = [col for col in numerical_cols if len(df[col].unique()) > 2]
+    df.loc[:, numerical_cols] = scaler.fit_transform(df[numerical_cols])
+
+    # Add back the "PassengerId" column in a way that avoids fragmentation
+    if passenger_id_column is not None:
+        df = pd.concat([passenger_id_column, df], axis=1)
+
+    return df, scaler
+
+def convert_to_numeric(df):
+    df = df.copy()
+    for col in df.columns:
+        if df[col].dtype == 'bool':
+            df.loc[:, col] = df[col].astype(int)
+    return df
+
+def split_and_save(train_df, test_df, train_path, test_path):
+    train_df.to_csv(train_path, index=False)
+    test_df.to_csv(test_path, index=False)
+
+
+
+def drop_constant_columns(df):
+    for col in df.columns:
+        unique_vals = df[col].unique()
+        if len(unique_vals) == 2 and (df[col].value_counts().min() == 1):
+            df.drop(col, axis=1, inplace=True)
+    return df
+
+def drop_non_numeric_text_columns(df):
+    dropped_cols = []
+    for col in df.columns:
+        if df[col].apply(lambda x: isinstance(x, str)).any():
+            dropped_cols.append(col)
+            df.drop(col, axis=1, inplace=True)
+    print(f"Dropped Columns: {dropped_cols}")
+    return df
+
+def knn_imputation(df):
+    imputer = KNNImputer(n_neighbors=3)
+    df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+    return df_imputed
+
+def main():
+    # Load and preprocess the data
+    combined_df = load_and_combine_data('/Users/scott/Downloads/titanic/train.csv', '/Users/scott/Downloads/titanic/test.csv')
+    combined_df = add_title_feature(combined_df)
+    combined_df = add_family_features(combined_df)
+    combined_df = add_new_features(combined_df)
+    
+    # One-hot encoding
+    combined_df = one_hot_encoding(combined_df, ['Sex', 'Embarked', 'Title', 'FamilyID', 'Deck', 'TicketPrefix', 'AgeBin_Class', 'AgeBin_Sex'])
+    
+    # Drop constant columns
+    combined_df = drop_constant_columns(combined_df)
+    
+    # Drop non-numeric text columns
+    combined_df = drop_non_numeric_text_columns(combined_df)
+    
+    # KNN Imputation
+    combined_df = knn_imputation(combined_df)
+    
+    # Convert to numeric (optional, for boolean values)
+    combined_df = convert_to_numeric(combined_df)
+
+    # Feature scaling
+    combined_df, _ = feature_scaling(combined_df)
+    
+    # Separate into train and test data
+    train_df = combined_df.loc[combined_df['Survived'].notna()].copy()
+    test_df = combined_df.loc[combined_df['Survived'].isna()].copy()
+
+    # Save the processed datasets
+    split_and_save(train_df, test_df, '/Users/scott/Downloads/titanic/train_engineered.csv', '/Users/scott/Downloads/titanic/test_engineered.csv')
+
+if __name__ == '__main__':
+    main()
